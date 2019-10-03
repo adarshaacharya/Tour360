634e4e9fcfa9cae6b6923535afa02202d2fb2034
Aadarsha <connectwithadarsha@gmail.com>
2019-10-03 11:27:32 +0545

    navbar issues fixed

---
+++ vendor/profile-page/360Images/main-page.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>360 VR</title>
+
+    
+    <link rel="icon" type="image/png" href="../../../img/favicon.ico" />
+
+    <!--Normalise CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../libs/normalize.css">
+
+    <!-- Font awesome 5.9-->
+    <link rel="stylesheet" href="../../../libs/fontawesome/css/all.css">
+
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="../../../libs/bootstrap.min.css">
+
+    <!--Material Design for Bootstrap -->
+    <link rel="stylesheet" href="../../../libs/mdb.min.css">
+
+    <link rel="stylesheet" href="../../../libs/carousel/owl.carousel.css">
+
+    <!--Style css-->
+    <link rel="stylesheet" type="text/css" media="screen" href="../../../css/style.css">
+    <link rel="stylesheet" href="../../../css/homestyle.css">
+
+    <!--Media Query -->
+    <link rel="stylesheet" type="text/css" href="../../../css/responsive.css">
+
+
+    <style>
+    
+      
+       .dropdown-item:hover {
+          background-color :black !important;
+          color:white !important
+      }
+    </style>
+</head>
+<body> 
+
+
+    
+        <!--Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">360 VR</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+          aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="./everest360.php" target="360view">Mt.Everest
+              </a>
+            </li>
+
+          </ul>
+
+          <ul class="navbar-nav ml-auto nav-flex-icons" style="margin-right : 120px;">
+        
+          
+                <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                          aria-haspopup="true" aria-expanded="false">Suggested
+                        </a>
+                        <div class="hover-drop dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                          <a class="hover-drop dropdown-item" href="./everest360.php" target="360view">Mt Everest</a>  
+                          <a class="hover-drop dropdown-item" href="./lumbini360.php" target="360view">Lumbini</a>
+                          <a class="hover-drop dropdown-item" href="./chitwan360.php" target="360view">Chitwan National Park</a>
+                          <a class="hover-drop dropdown-item" href="./bunjee360.php" target="360view">Bunjee Jumping</a>
+                          <a class="hover-drop dropdown-item" href="./zipFly360.php" target="360view">Zip Flyer</a>
+                          <a class="hover-drop dropdown-item" href="./patanDurbar360.php" target="360view">Patan Durbar Square</a>
+                          <a class="hover-drop dropdown-item" href="./pashupati360.php" target="360view">Pashupatinath Temple</a>
+                        </div>
+                      </li>
+          </ul>
+        </div>
+      </nav>
+
+      <!--/.Navbar -->
+
+
+
+       
+
+
+     <iframe src="everest360.php" frameborder="0" style="height:100vh;width:100%;" name="360view"></iframe>
+
+
+
+
+      <!--Jquery JS-->
+      <script src="../../../libs/jquery-3.4.1.min.js"></script>
+
+<!--Bootstrap JS-->
+<script src="../../../libs/bootstrap.min.js"></script>
+
+
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="../../../libs/mdb.min.js"></script>
+
+
+<!--Popper JS-->
+<script src="../../../libs/popper.min.js"></script>
+
+<script src="../../../libs/carousel/owl.carousel.min.js"></script>
+
+
+<!--Custom JS -->
+<script src="../../../js/custom.js"></script>
+
+
+<!--Extra JS Libraries-->
+<script src="../../js/hoverIntent.js"></script>
+<script src="../../js/superfish.min.js"></script>
+<script src="../../js/jquery.ajaxchimp.min.js"></script>
+<script src="../../js/jquery.magnific-popup.min.js"></script>
+<script src="../../js/jquery.nice-select.min.js"></script>
+
+
+            
+    <!-- <script src="aframe.js"></script> -->
+<script src="https://aframe.io/releases/0.9.2/aframe.min.js"></script>
+<script src="https://unpkg.com/aframe-event-set-component@3.0.3/dist/aframe-event-set-component.min.js"></script>
+    
+</body>
+</html>
+
